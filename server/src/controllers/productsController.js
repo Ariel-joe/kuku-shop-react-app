b727759591fda90b00ad23574ea685944b5d27fc
Ariel-Joe <arieljoe00@gmail.com>
2025-02-06 22:25:46 +0300

    controllers update

--- server/src/controllers/productsController.js
+++ server/src/controllers/productsController.js
@@ -1,5 +1,24 @@
-export const getproducts = (req, res) => {
-  res.json({
-    messsage: "this is the products route",
-  });
-}
+import { Product } from "../database/models/product.js";
+
+// creating a product
+export const addproduct = async (req, res) => {
+  try {
+    const { name, description, image, price } = req.body;
+
+    
+  } catch (error) {}
+};
+
+// fetching all the products from the database.
+export const getproducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    return res.status(200).json({
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
