6d8116df8a08166480699f423a7fcd1b5deba49e
Ariel-Joe <arieljoe00@gmail.com>
2025-02-06 22:18:15 +0300

    model design

--- server/src/database/models/product.js
+++ server/src/database/models/product.js
@@ -1,7 +1,10 @@
 import { model, Schema } from "mongoose";
 
 const productSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true },
+});
 
-})
-
-export const Product = model('product', productSchema)
+export const Product = model("product", productSchema);
